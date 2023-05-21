73b523800be295d928190de3c66ee3afede427e0
IT21034886 <87615770+IT21034886@users.noreply.github.com>
2023-05-21 10:39:38 +0530

    updated

---
+++ routes/company.js
@@ -0,0 +1,26 @@
+import express, { Router } from "express";
+
+import {
+  addCompany,
+  getAllCompanies,
+  getNumOfCompanies,
+  getCompaniesOfAType,
+  deleteCompany,
+  editCompany,
+  getCompany,
+  findSellerByEmail,
+} from "../controllers/company.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+const router = express.Router();
+
+router.post("/", catchAsync(addCompany));
+router.get("/", catchAsync(getAllCompanies));
+router.get("/getProductsOfAType/:type", catchAsync(getCompaniesOfAType));
+router.delete("/:id", catchAsync(deleteCompany));
+router.get("/getNumOf", catchAsync(getNumOfCompanies));
+router.patch("/:id", catchAsync(editCompany));
+router.get("/:id", catchAsync(getCompany));
+router.get("/find/:email", catchAsync(findSellerByEmail));
+
+export default router;
